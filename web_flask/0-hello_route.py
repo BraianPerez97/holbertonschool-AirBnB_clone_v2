c5f3b0ab3f4827f736a6873f851a3c50e42677ad
BraianPerez97 <3603@holbertonstudents.com>
2023-08-17 15:51:21 -0400

    adding documantation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+# import Flask class from flask module
 from flask import Flask
-""" Hello Flask """
 
-
+# Create an instance of Flask class and assing it to var 'app'
 app = Flask(__name__)
+
+# Define root URL ('/') ans disable stric_slashes
 
 
 @app.route('/', strict_slashes=False)
-"""Hello HBND"""
-
-
 def hello_hbnd():
+    # Function is executed when access to URL
     return "Hello HBND!"
 
+
 if __name__ == "__main__":
+    # Run flask app, making it listen to 0.0.0.0 and port 5000
     app.run(host='0.0.0.0', port=5000)
