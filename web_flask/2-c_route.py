750cba0f0c240202c40368b3485986f6e259a67a
BraianPerez97 <3603@holbertonstudents.com>
2023-08-17 18:31:34 -0400

    c

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Start Flask webapp """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Return hello"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return ("HBNB")
+
+
+@app.route("/c/<tetx>", strict_slashes=False)
+def c_fun(text):
+    """returns c variable"""
+    return "C " + text.replace('_', " ")
+
+
+if __name__ == "__main__":
+    """runs"""
+    app.run(host='0.0.0.0', port='5000')
