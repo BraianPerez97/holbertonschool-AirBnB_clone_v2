9ac012d868b6f8e08c3ac543b7ce126ad1a570b5
BraianPerez97 <3603@holbertonstudents.com>
2023-08-18 15:28:27 -0400

    adding template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,43 @@
 #!/usr/bin/python3
 """ Start Flask webapp """
-from flask import Flask, render_template
-
-
+from flask import Flask, render_template, escape
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
     """Return hello"""
     return ("Hello HBNB!")
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hello_hbnb():
     """Return HBNB"""
     return ('HBNB')
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_fun(text):
     """returns c variable"""
-    return "C " + text.replace('_', " ")
+    return "C {}".format(escape(text).replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/')
+@app.route('/python/<text>')
 def python_cool(text="is cool"):
-    if text is not "is cool":
-        text = text.replace('_', " ")
-    return f"Python {text}"
+    return "Python {}".format(escape(text).replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def int_n(n):
     """print int"""
-    if type(n) is int:
-        return f"{n} is a number"
-    else:
-        raise TypeError
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def html(n):
-    if type(n) is int:
-        return render_template('5-number.html', number=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
