cd8bb389d03f31e756738eafc167a1708985e365
BraianPerez97 <3603@holbertonstudents.com>
2023-08-18 10:15:18 -0400

    is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Start Flask webapp """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Return hello"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Return HBNB"""
+    return ('HBNB')
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    """returns c variable"""
+    return "C " + text.replace('_', " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_cool(text="is cool"):
+    if text is not "is cool":
+        text = text.replace('_', " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_n(n):
+    """print int"""
+    if type(n) is int:
+        return f"{n} is a number"
+    else:
+        raise TypeError
+
+
+if __name__ == "__main__":
+    """runs"""
+    app.run(host='0.0.0.0', port='5000')
