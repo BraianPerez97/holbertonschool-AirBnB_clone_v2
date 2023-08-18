58616ec21ac8795543938b8db28a898f6bbdeb3c
BraianPerez97 <3603@holbertonstudents.com>
2023-08-18 14:23:57 -0400

    typos

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,54 +5,59 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello():
-    """Return hello"""
-    return ("Hello HBNB!")
+@app.route('/', strict_slashes=False)
+def hell():
+    """function to return a string"""
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
-    """Return HBNB"""
-    return ('HBNB')
+    """prints hbnb"""
+    return "HBNB"
 
 
-@app.route("/c/<text>")
-def c_fun(text):
-    """returns c variable"""
-    return "C " + text.replace('_', " ")
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """"replace _ with spaces"""
+    text = text.replace('_', ' ')
+    return f"c {text}"
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_cool(text="is cool"):
-    if text is not "is cool":
-        text = text.replace('_', " ")
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is_cool"):
+    """print is cool"""
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
     return f"Python {text}"
 
 
-@app.route('/number/<int:n>')
-def int_n(n):
-    """print int"""
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_int(n):
+    """"print if is int"""
     if type(n) is int:
-        return "{} is a number".format(n)
+        return f"{n} is a number"
+    else:
+        raise TypeError
 
 
-@app.route('/number_template/<int:n>')
-def number_template(n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_page(n):
     """print html"""
-    return render_template('5-number.html', n=n)
+    if type(n) is int:
+        return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<int:n>')
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def is_odd_or_even(n):
     """route that evaluates if n is odd or even"""
-    def number_odd_or_even(n):
-        is_odd_or_even = "odd" if n % 2 != 0 else "even"
-        return render_template('6-number_odd_or_even.html', n=n,
-                               is_odd_or_even=is_odd_or_even)
+    if type(n) is int:
+        if n % 2 == 0:
+            return render_template('6-number_odd_or_even.html', number=n, text='odd')
+    else:
+        raise TypeError
 
 
-if __name__ == "__main__":
-    """runs"""
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
