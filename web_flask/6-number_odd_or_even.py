600fe1b4da696ac4207a6927e6c4e1fe2fcad130
BraianPerez97 <3603@holbertonstudents.com>
2023-08-18 13:46:35 -0400

    flask instance created

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 """ Start Flask webapp """
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
     """Return hello"""
     return ("Hello HBNB!")
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hello_hbnb():
     """Return HBNB"""
     return ('HBNB')
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_fun(text):
     """returns c variable"""
     return "C " + text.replace('_', " ")
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/')
+@app.route('/python/<text>')
 def python_cool(text="is cool"):
     if text is not "is cool":
         text = text.replace('_', " ")
     return f"Python {text}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def int_n(n):
     """print int"""
     if type(n) is int:
@@ -41,20 +40,19 @@ def int_n(n):
         raise TypeError
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def html(n):
     """print html"""
     if type(n) is int:
-        return render_template('5-number.html', number=n)
+        return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def is_odd_or_even(n):
     """route that evaluates if n is odd or even"""
     if type(n) is int:
         if n % 2 == 0:
-            return render_template('6-number_odd_or_even.html',
-                                   number=n, text='odd')
+            return render_template('6-number_odd_or_even.html', n=n)
     else:
         raise TypeError
 
