2ff97b8fe45055001b72d843d03241eeea5b9823
BraianPerez97 <3603@holbertonstudents.com>
2023-08-18 15:59:33 -0400

    edit file and db storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts Flask"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    """Runs"""
+    app.run(host='0.0.0.0', port=5000)
